c2981c1cdf6525dbffe5a690a23d52dee0a35e83
aanddy36 <anchibro@hotmail.com>
2023-06-07 11:07:00 -0500

    You can already switch among pages, seeing not only the top 20 actors, but the first 2000. The only aspect left is to make responsive the website: change number of actors in a row when screen size reduces

--- app.js
+++ app.js
@@ -3,27 +3,60 @@ let btns = document.querySelectorAll(".decision button");
 //let btnMovies = document.querySelector(".btn-movies");
 //let btnActors = document.querySelector(".btn-actors");
 let specialWord = document.querySelector("#title-acent");
-
+let btnPages = document.querySelectorAll(".btn-page");
+let actualPage = document.querySelector(".current p");
+let page = 1;
+let selectedOption = "movie";
+btnPages.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        if(btn.classList[1] === "prior"){
+            if(page>1){
+                page = Number(actualPage.innerHTML)-1;
+                actualPage.innerHTML = page;
+                btns.forEach((btn)=>{
+                    if(btn.classList[1] === "selected-btn"){
+                        loadMoviesOrActors(btn.classList[0], page);
+                    }
+                })
+            }
+        }else if(btn.classList[1] === "next"){
+            if(page < 100){
+                page = Number(actualPage.innerHTML)+1;
+                actualPage.innerHTML = page;
+                btns.forEach((btn)=>{
+                    if(btn.classList[1] === "selected-btn"){
+                        loadMoviesOrActors(btn.classList[0], page);
+                    }
+                })
+            }}
+    });
+});
 btns.forEach((btn)=>{
     btn.addEventListener("click",()=>{
         if(!(btn.classList[1]=== "selected-btn")){
             btns.forEach(btn=>btn.classList.remove("selected-btn"));
             btn.classList.add("selected-btn");
         }
-        if(btn.classList[0]==="btn-actors"){
+        if(btn.classList[0]==="person"){
+            page = 1;
+            actualPage.innerHTML = page;
             specialWord.innerHTML = "actors";
-            loadMoviesOrActors("person");
+            selectedOption = "person";
+            loadMoviesOrActors(selectedOption, page);
         }else{
+            page = 1;
+            actualPage.innerHTML = page;
             specialWord.innerHTML = "movies";
-            loadMoviesOrActors("movie");
+            selectedOption = "movie";
+            loadMoviesOrActors(selectedOption, page);
         }
     });
 
 })
 
-async function loadMoviesOrActors(movieOrPerson){
+async function loadMoviesOrActors(movieOrPerson, page){
     try{
-        const response = await fetch(`https://api.themoviedb.org/3/${movieOrPerson}/popular?api_key=2e7092078ba03dac9c58a1825ce925e1&language=en-US&page=1`);
+        const response = await fetch(`https://api.themoviedb.org/3/${movieOrPerson}/popular?api_key=2e7092078ba03dac9c58a1825ce925e1&language=en-US&page=${page}`);
         if(response.status === 200){
             let data = await response.json();
             data = data.results;
@@ -33,7 +66,7 @@ async function loadMoviesOrActors(movieOrPerson){
                 data.forEach((movie, ranking) =>{
                     text += `<div class="movies">
                     <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="movies-image">
-                    <p class="movies-ranking">${ranking+1}</p>
+                    <p class="movies-ranking">${ranking+1+(20*(page-1))}</p>
                     <h1 class="movies-titles">${movie.title}</h1>
                 </div>`
                 })
@@ -41,7 +74,7 @@ async function loadMoviesOrActors(movieOrPerson){
                 data.forEach((actor, ranking) =>{
                     text += `<div class="movies">
                     <img src="https://image.tmdb.org/t/p/w500/${actor.profile_path}" class="movies-image">
-                    <p class="movies-ranking">${ranking+1}</p>
+                    <p class="movies-ranking">${ranking+1+(20*(page-1))}</p>
                     <h1 class="movies-titles">${actor.name}</h1>
                 </div>`
                 })
@@ -55,7 +88,7 @@ async function loadMoviesOrActors(movieOrPerson){
         console.log("Something went wrong");
     }
 }
-loadMoviesOrActors("movie");
+loadMoviesOrActors("movie", 1);
 
 //async function loadMoviesOrActors(){
 //    try{
